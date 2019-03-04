611aa2944cbc606eff820ca40a7046352f50a650
Mohamed Zaki <mohamed_zaki888@live.com>
2019-03-04 21:03:29 +0200

    nothing usefull

--- Game News/More Details/DetailedViewController.swift
+++ Game News/More Details/DetailedViewController.swift
@@ -10,39 +10,38 @@ import UIKit
 
 class DetailedViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        let barView = UIView()
+        view.addSubview(barView)
+        barView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
+        barView.trailingAnchor.constraint(equalTo: imageView.trailingAnchor).isActive = true
+        barView.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        barView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 100, y: 200))
-        path.addLine(to: CGPoint(x: 200, y: 400))
-        path.addLine(to: CGPoint(x: 10, y: 400))
-        path.close()
-        path.fill()
+        path.move(to: CGPoint(x: barView.frame.minX, y: 100))
+        path.addLine(to: CGPoint(x: barView.frame.minX, y: 0))
+//        path.addLine(to: CGPoint(x: 10, y: 400))
+//        path.close()
+//        path.fill()
         path.stroke()
         let shapedLayer = CAShapeLayer()
-        shapedLayer.strokeColor = UIColor.red.cgColor
+        shapedLayer.strokeColor = UIColor.blue.cgColor
         shapedLayer.path = path.cgPath
-        view.layer.addSublayer(shapedLayer)
+//        view.layer.addSublayer(shapedLayer)
         shapedLayer.lineWidth = 10
         let animation = CABasicAnimation(keyPath: "strokeEnd")
         animation.fromValue = 0.0
         animation.toValue = 1.0
         animation.duration = 2
         shapedLayer.add(animation, forKey: "drawLine")
+        barView.layer.addSublayer(shapedLayer)
+        barView.translatesAutoresizingMaskIntoConstraints = false
+
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
